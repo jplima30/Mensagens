9db1d43df1441aa8d75c4b42c63e0a49efb0c510
System Administrator <root@jplimas-iMac.local>
2022-07-18 13:11:23 -0300

    Sync_Repo

---
+++ Mensagens/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by jplima on 18/07/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
